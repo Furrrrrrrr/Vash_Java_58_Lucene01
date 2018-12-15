394c8cc183cee18f839fd04d0a0e2762a690ac42
Furrrrrrrr <38304569+Furrrrrrrr@users.noreply.github.com>
2018-12-15 23:33:09 +0800

    remote changes
    change test

--- src/main/java/cn/itcast/lucene/TestGit.java
+++ src/main/java/cn/itcast/lucene/TestGit.java
@@ -4,6 +4,9 @@ public class TestGit {
 
     public static void main(String[] args) {
         System.out.println("try to push to the remote");
+        
+        System.out.println("remote accepted");
+        
     }
 
 }
